f548bc44a8155acf75851df74b3e1f1b069a6af0
chrismwendt <chrismwendt@gmail.com>
2014-05-03 16:34:26 -0400

    Remove comment

--- src/Winterbells.scala
+++ src/Winterbells.scala
@@ -4,7 +4,6 @@ import java.awt.GraphicsDevice.WindowTranslucency._
 
 object Winterbells {
   def main(args: Array[String]): Unit = {
-    // create the overlay on the event-dispatching thread
     SwingUtilities.invokeLater(new Runnable {
       def run() {
         new Overlay().setVisible(true)
