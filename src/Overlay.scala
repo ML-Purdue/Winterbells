4e179206d7131228ec47872b19b8c245f64d603d
chrismwendt <chrismwendt@gmail.com>
2014-05-03 15:43:52 -0400

    Draw an oval

--- src/Overlay.scala
+++ src/Overlay.scala
@@ -9,14 +9,9 @@ class Overlay extends JFrame {
 
   override def paint(g: Graphics) {
     if (g.isInstanceOf[Graphics2D]) {
-      val R = 240;
-      val G = 240;
-      val B = 240;
-
-      val p = new GradientPaint(0.0f, 0.0f, new Color(R, G, B, 0), 0.0f, getHeight(), new Color(R, G, B, 255), true);
-      val g2d = g.asInstanceOf[Graphics2D];
-      g2d.setPaint(p);
-      g2d.fillRect(0, 0, getWidth(), getHeight());
+	  val g2d = g.asInstanceOf[Graphics2D];
+	  g2d.setColor(Color.RED)
+      g2d.fillOval(50, 50, 10, 10)
     }
   }
 }
